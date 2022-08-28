050843630722beecc91b38c61cdadf1d9e27fd9d
Voropanov Denis <voropanovdn@gmail.com>
2022-08-28 12:25:54 +0300

    Initial Commit

---
+++ ARGlasses/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  ARGlasses
+//
+//  Created by Alexandra Popova on 28.08.2022.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
